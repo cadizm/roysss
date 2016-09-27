2dae60b65859737c7c99e7e76e56c207bfaf8554
Michael Cadiz <mcadiz@truecar.com>
2016-09-27 14:57:18 -0700

    DJANGO_SECRET_KEY

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ instance that mimics the production vps host (e.g. digital ocean).
 Do a `vagrant up` and set up a local user that has `sudo`.
 
 All other interaction should be done via ansible.
+
+
+Environment Variables
+=====================
+
+These need to be set manually on a per-user basis.
+
+* `DJANGO_SECRET_KEY` (root)
--- roysss/roysss/settings.py
+++ roysss/roysss/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's4k7e^s3))fk+jiafdog*_w$&%ackug0%pp%jy_x8cw+u0p+d!'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
