fe9d12abc1b768b54c890b334e2ad3749616ae8a
Michael Cadiz <mcadiz@truecar.com>
2016-10-14 14:31:49 -0700

    use stripe test keys on vagrant box

--- roysss/roysss/settings/production.py
+++ roysss/roysss/settings/production.py
@@ -2,8 +2,14 @@
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
+import socket
+
 from common import *
 
 
 STRIPE['SECRET_KEY'] = secrets['STRIPE_LIVE_SECRET_KEY']
 STRIPE['PUBLISHABLE_KEY'] = secrets['STRIPE_LIVE_PUBLISHABLE_KEY']
+
+if 'vagrant' in socket.gethostname():
+    STRIPE['SECRET_KEY'] = secrets['STRIPE_TEST_SECRET_KEY']
+    STRIPE['PUBLISHABLE_KEY'] = secrets['STRIPE_TEST_PUBLISHABLE_KEY']
