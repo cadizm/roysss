724637e5b9131ef5d2588775aa8c6f15c6c5bee9
Michael Cadiz <mcadiz@truecar.com>
2016-10-23 15:37:50 -0700

    TEMP: production uses sqlite database

--- roysss/roysss/settings/production.py
+++ roysss/roysss/settings/production.py
@@ -13,3 +13,11 @@ STRIPE['PUBLISHABLE_KEY'] = secrets['STRIPE_LIVE_PUBLISHABLE_KEY']
 if 'vagrant' in socket.gethostname():
     STRIPE['SECRET_KEY'] = secrets['STRIPE_TEST_SECRET_KEY']
     STRIPE['PUBLISHABLE_KEY'] = secrets['STRIPE_TEST_PUBLISHABLE_KEY']
+
+# TODO: remove me
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
